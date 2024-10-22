c7f08d0a1431fefce2e590e83a2bd2a0ffa820b0
Hezrongg <hezrongg26@gmail.com>
2024-10-22 21:50:29 +0800

    initial commit

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Ford",
+    model: "Mustang",
+    color: "Black"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
